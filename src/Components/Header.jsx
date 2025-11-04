8d1099d2fa17a25dc9a6d41dd234b756aea33184
Ademuyiwa Obaloluwa <obaobasky7@gmail.com>
2025-11-04 14:11:59 +0100

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <nav className="bg-pink-700 flex justify-around items-center py-4 sticky shadow-2xl">
+      <nav className="bg-pink-700 flex md:justify-around justify-center md:gap-0 gap-7 items-center py-4 sticky shadow-2xl">
         <div className="flex justify-center items-center gap-3">
           <img src="vite.svg" alt="" className="w-10 h-10" />
           <h1 className="font-bold text-xl ">Vite</h1>
         </div>
-        <div>
+        <div className="md:block hidden">
           <ol className="text-blue-600 underline flex justify-center items-center gap-10">
             <li>Home</li>
             <li>Jobs</li>
@@ -17,7 +17,7 @@ function Header() {
             <li>Applications</li>
           </ol>
         </div>
-        <button className="bg-white px-4 py-1 rounded-xl">Login</button>
+        <button className="bg-transparent hover:bg-white px-4 py-1 rounded-xl text-white text-xl border-2 hover:text-pink-700">Login</button>
       </nav>
     </div>
   );
