cf21b3a4ba1a80e39b8412ff7497c9d73c5b316e
Ademuyiwa Obaloluwa <obaobasky7@gmail.com>
2025-11-04 14:07:09 +0100

    first commit

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,11 +4,11 @@ import Cards from './Card';
 function Body() {
   return (
     <div>
-      <div className="text-white text-center bg-pink-700 p-20">
+      <div className="text-white text-center bg-pink-700 py-20">
         <h1 className="font-bold text-5xl mb-5">Become a React Dev</h1>
         <p>Find the React job that fits your skill set</p>
       </div>
-      <div className='flex justify-center items-center gap-[150px] mt-16'>
+      <div className='md:flex block justify-center items-center gap-[150px] mt-16'>
         <Cards
           title='For Developers'
         description='Browse our React jobs and start your career today'/>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cards({title, description}) {
   return (
     <div>
-      <div className='border border-gray-400 shadow-2xl w-[550px] rounded-xl p-7'>
+      <div className='border border-gray-400 shadow-2xl md:w-[550px] w-[470px] m-auto rounded-xl p-7 mb-10'>
         <h1 className='font-bold text-3xl mb-4'>{title}</h1>
         <p className='font-medium text-base'>{description}</p>
       </div>
